90623249318e31ac73071b5a2698112a3c03ee0f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-06-11 15:12:50 +0200

    Allow upload snapshots to maven central

--- src/auth.ts
+++ src/auth.ts
@@ -80,12 +80,55 @@ export function generate(
       '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
       '@xsi:schemaLocation':
         'http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd',
+      profiles: [{
+        profile: {
+          id: '_sonatype-snapshots_',
+          activation: {
+            activeByDefault: true
+          },
+          repositories: [{
+            repository: {
+              id: 'sonatype-snapshots',
+              url: 'https://s01.oss.sonatype.org/content/repositories/snapshots',
+              releases: {
+                enabled: false
+              },
+              snapshots: {
+                enabled: true
+              }
+            }
+          }],
+          pluginRepositories: [{
+            pluginRepository: {
+              id: 'sonatype-snapshots',
+              url: 'https://s01.oss.sonatype.org/content/repositories/snapshots',
+              releases: {
+                enabled: false
+              },
+              snapshots: {
+                enabled: true
+              }
+            }
+          }]
+        }
+      }],
       servers: {
         server: [
           {
             id: id,
             username: `\${env.${username}}`,
             password: `\${env.${password}}`
+          }
+        ]
+      },
+      mirrors: {
+        mirror: [
+          {
+            id: 'maven-default-http-blocker',
+            mirrorOf: 'external:http:*',
+            name: 'Pseudo repository to mirror external repositories initially using HTTP.',
+            url: 'https://repo.maven.apache.org/maven2',
+            blocked: false
           }
         ]
       }
